95baba2fa6c942ba00247d8aab91c66567c6be1b
Nikolay Nozdrin-Plotnitsky <nozdrin.plotnitsky@gmail.com>
2018-02-28 16:03:44 +0300

    some corrections for start work

---
+++ reports/src/components/ProposalDAO.vue
@@ -0,0 +1,48 @@
+<template>
+  <div id="proposal-dao">
+    <b-table
+      :data="isEmpty ? [] : tableData"
+      :bordered="isBordered"
+      :striped="isStriped"
+      :narrowed="isNarrowed"
+      :loading="isLoading"
+      :paginated="isPaginated"
+      :per-page="perPage"
+      :current-page.sync="currentPage"
+      :pagination-simple="isPaginationSimple"
+      :mobile-cards="hasMobileCards">
+      <template slot-scope="props">
+        <b-table-column label='Type' centered>
+          {{ props.row.type }}
+        </b-table-column>
+        <b-table-column label='Date' centered>
+          {{ props.row.date }}
+        </b-table-column>
+        <b-table-column label='State' centered>
+          {{ props.row.state }}
+        </b-table-column>
+      </template>
+      </b-table>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ['tableData'],
+  data () {
+    return {
+      isEmpty: false,
+      isBordered: false,
+      isStriped: true,
+      isNarrowed: false,
+      isLoading: false,
+      hasMobileCards: true,
+      isPaginated: false,
+      isPaginationSimple: false,
+      currentPage: 1,
+      perPage: 5
+    }
+  }
+}
+</script>
+
--- reports/src/pages/DAO.vue
+++ reports/src/pages/DAO.vue
@@ -1,10 +1,10 @@
 <template>
     <div>
     <section class="allMain">
-      <div class="h2-contain">           
+      <div class="h2-contain">
         <h2 class="subtitle">DAO Proposal</h2>
       </div>
-      <search-results :tableData='searchData'></search-results>
+      <proposal-dao :tableData='proposalData'></proposal-dao>
       <b-loading :active.sync="isLoading" :canCancel="true"></b-loading>
     </section>
     </div>
@@ -13,75 +13,29 @@
 
 
 <script>
-import SearchResults from '@/components/SearchResults'
-import BRadioButton from 'buefy/src/components/radio/RadioButton'
-// replace it to dao data
+import ProposalDao from '@/components/ProposalDAO'
 export default {
   data () {
     return {
-      reportAddress: this.$eth.reportAddress,
-      reportText: '',
-      owner: false,
-      reportNumber: 0,
-      searchData: [],
+      proposalData: [],
       isLoading: false
     }
   },
   methods: {
-    async search () {
-      this.searchReports()
-    },
-    async newReport () {
-      try {
-        await this.$eth.addNewReport(this.reportText)
-      } catch (err) {
-        console.log(err)
-      }
-    },
-    async searchReports () {
-      this.searchData = []
-      this.isLoading = true
-      try {
-        let j = await this.$eth.reportCounter()
-        for (let i = j - 1; i > 0; --i) {
-          let report = await this.$eth.getReport(i)
-          console.log(report)
-          this.searchData.push({date: new Date(report[1] * 1000).toLocaleString(), report: report[0]})
-        }
-      } catch (err) {
-        console.log(err)
-      }
-      this.isLoading = false
-    },
-    async loadReport () {
-      this.reportNumber = await this.$eth.reportCounter()
-    },
-    async loadETH () {
-      this.isLoading = true
-      try {
-        await this.loadReport()
-      } catch (err) {
-        console.log(err)
-      }
-      this.isLoading = false
-    },
-    async checkOwner () {
-      this.owner = await this.$eth.isOwner()
+    async loadProposals () {
+      this.proposalData.push({date: new Date(), state: 'New', type: 'Hello everyone!'})
+      this.proposalData.push({date: new Date(), state: 'Old', type: 'Good Bye!'})
     }
   },
   created () {
     try {
-      this.loadETH()
-      this.loadReport()
-      this.search()
-      this.checkOwner()
+      this.loadProposals()
     } catch (err) {
       console.log(err)
     }
   },
   components: {
-    SearchResults,
-    BRadioButton
+    ProposalDao
   }
 }
 </script>
