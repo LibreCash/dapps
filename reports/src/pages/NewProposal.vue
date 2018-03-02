8d60497e55c0d8802a04580b72bf8bf9ec2412ce
Nikolay Nozdrin-Plotnitsky <nozdrin.plotnitsky@gmail.com>
2018-03-02 15:34:19 +0300

    Create New Proposal....WIP

--- reports/src/pages/NewProposal.vue
+++ reports/src/pages/NewProposal.vue
@@ -5,90 +5,197 @@
         <h2 class="subtitle">DAO New Proposal</h2>
       </div>
       <br>
-      <table>
-        <tr>
-          <th>Type Proposal:</th>
-          <td>
-            <select v-model="selectTypeProposal" @click="setType">
-              <option v-for="type in typeProposals" v-bind:value="type.value">
+      <div class="table-padding" >
+        <b-field horizontal label="Type" >
+          <b-select placeholder="Select a type proposal" v-model="selectedType">
+              <option v-for="type in typeProposals" v-bind:value="type">
                 {{ type.text }}
               </option>
-            </select>
-          </td>
-        </tr>
-        <tr>
-          <th>Beneficiary:</th>
-          <td><input v-model="beneficiary"></td>
-        </tr>
-        <tr>
-          <th>Buffer:</th>
-          <td><input v-model="buffer"></td>
-        </tr>
-        <tr>
-          <th>Send wei:</th>
-          <td><input v-model="weiAmount"></td>
-        </tr>
-        <tr>
-          <th>Description:</th>
-          <td><textarea v-model="description"></textarea></td>
-        </tr>
-        <tr>
-          <th>Debating Period (in minutes):</th>
-          <td><input v-model="debatingPeriodInMinutes"></td>
-        </tr>
-        <tr>
-          <th>Bytecode:</th>
-          <td><textarea v-model="transactionBytecode"></textarea></td>
-        </tr>
-        <tr colspan="2"><button v-on:click="createProposal()">Create Proposal</button></tr>
-      </table>
+          </b-select>
+        </b-field>
+        <b-field horizontal :label="selectedType['benef']" v-if="selectedType['benef']"
+          :type="typeBenef">
+            <b-input v-model="beneficiary" placeholder="0x0000000000000000000000000000000000000000"></b-input>
+        </b-field>
+        <b-field horizontal :label="selectedType['buf']" v-if="selectedType['buf']">
+            <b-input v-model="buffer"></b-input>
+        </b-field>
+        <b-field horizontal :label="selectedType['amount']" v-if="selectedType['amount']">
+            <b-input v-model="weiAmount"></b-input>
+        </b-field>
+        <b-field horizontal label="Description">
+            <b-input type="textarea" v-model="description"></b-input>
+        </b-field>
+        <b-field horizontal label="Debating">
+            <b-input v-model="debatingPeriodInMinutes"></b-input>
+        </b-field>
+        <b-field horizontal :label="selectedType['code']" v-if="selectedType['code']">
+            <b-input type="textarea" v-model="transactionBytecode"></b-input>
+        </b-field>
+        <b-field horizontal>
+            <p class="control">
+                <button class="button is-primary" v-on:click="createProposal()">
+                  Create Proposal
+                </button>
+            </p>
+        </b-field>
+      </div>
     </section>
     </div>
 </template>
 
 <script>
-
 export default {
   data () {
     return {
+      proposalData: [],
       daoAddress: this.$eth.daoAddress,
-      selectTypeProposal: '',
       beneficiary: '',
+      typeBenef: 'is-danger',
       weiAmount: '',
       description: '',
       debatingPeriodInMinutes: '',
       transactionBytecode: '',
       buffer: '',
       typeProposals: [
-        {text: 'Clean', value: 0}, // CLEAN
-        {text: 'Universal', value: 1}, // UNIVERSAL
-        {text: 'Transfer ownership', value: 2}, // TRANSFER_OWNERSHIP
-        {text: 'Set buy limits', value: 3}, // SET_BUY_LIMITS
-        {text: 'Set sell limits', value: 4}, // SET_SELL_LIMITS
-        {text: 'Cancel buy order', value: 5}, // CANCEL_BUY_ORDER
-        {text: 'cancel sell order', value: 6}, // CANCEL_SELL_ORDER
-        {text: 'Attach token', value: 7}, // ATTACH_TOKEN
-        {text: 'Set bank address', value: 8}, // SET_BANK_ADDRESS
-        {text: 'Set relevance period', value: 9}, // RELEVANCE_PERIOD
-        {text: 'Set queue period', value: 10}, // QUEUE_PERIOD
-        {text: 'Set fees', value: 11}, // SET_FEES
-        {text: 'Add oracle', value: 12}, // ADD_ORACLE
-        {text: 'Disable oracle', value: 13}, // DISABLE_ORACLE
-        {text: 'Enable oracle', value: 14}, // ENABLE_ORACLE
-        {text: 'Delete oracle', value: 15}, // DELETE_ORACLE
-        {text: 'Set scheduler', value: 16}, // SET_SCHEDULER
-        {text: 'Winthdraw balance', value: 17} // WITHDRAW_BALANCE
-      ]
+        //{text: 'Clean', key: 'CLEAN', fields:[]}, // 
+        {text: 'Universal', key: 'UNIVERSAL', benef:'Beneficiary',amount:'Amound Wei',code:'Bytecode'},
+        {text: 'Transfer ownership', key: 'TRANSFER_OWNERSHIP', benef:'New Owner'},
+        {text: 'Set buy limits', key:'SET_BUY_LIMITS', amount:'Min Buy In Wei',buf:'Max Buy In Wei'},
+        {text: 'Set sell limits', key:'SET_SELL_LIMITS', amount:'Min Sell In Wei',buf:'Max Sell In Wei'},
+        {text: 'Cancel buy order', key:'CANCEL_BUY_ORDER', amount:'Order ID'},
+        {text: 'cancel sell order', key: 'CANCEL_SELL_ORDER', amount:'Order ID'},
+        {text: 'Attach token', key: 'ATTACH_TOKEN', benef:'Token Address'},
+        {text: 'Set bank address', key:'SET_BANK_ADDRESS', benef:'Bank Address'},
+        {text: 'Set relevance period', key: 'RELEVANCE_PERIOD', amount:'Period in seconds'},
+        {text: 'Set queue period', key: 'QUEUE_PERIOD', amount:'Period in seconds'},
+        {text: 'Set fees', key: 'SET_FEES', amount:'Buy fee', buf: 'Sell fee'},
+        {text: 'Add oracle', key: 'ADD_ORACLE', benef:'Oracle Address'},
+        {text: 'Disable oracle', key: 'DISABLE_ORACLE', benef:'Oracle Address'},
+        {text: 'Enable oracle', key: 'ENABLE_ORACLE', benef:'Oracle Address'},
+        {text: 'Delete oracle', key: 'DELETE_ORACLE', benef:'Oracle Address'},
+        {text: 'Set scheduler', key: 'SET_SCHEDULER', benef:'Scheduler Address'},
+        {text: 'Winthdraw balance', value: 'WITHDRAW_BALANCE'}
+      ],
+      selectedType: {text: 'Select a type proposal', fields: []}
     }
   },
   methods: {
     setType() {
       console.log(this.selectTypeProposal)
     },
-    createProposal() {
-      console.log("Create proposal")
-      //await this.$eth.daoContract.newProposal(selectTypeProposal,beneficiary,weiAmount,description,
-      //                                    debatingPeriodInMinutes,transactionBytecode)
+    async createProposal() {
+      console.log("Create proposal",this.selectedType)
+
+      switch(this.selectedType.key) {
+        //case 'CLEAN': break;
+        case 'UNIVERSAL':
+          await this.$eth._daoContract.proposalUniversal(
+            this.beneficiary, 
+            this.weiAmount,
+            this.description,
+            this.debatingPeriodInMinutes,
+            this.transactionBytecode)
+          break;
+        case 'TRANSFER_OWNERSHIP':
+          await this.$eth._daoContract.proposalTransferOwnership(
+            this.beneficiary, 
+            this.description,
+            this.debatingPeriodInMinutes)
+          break;
+        case 'SET_BUY_LIMITS':
+          await this.$eth._daoContract.proposalSetBuyLimits(
+            this.weiAmount,
+            this.buffer,
+            this.description,
+            this.debatingPeriodInMinutes)
+          break;
+        case 'SET_SELL_LIMITS':
+          await this.$eth._daoContract.proposalSetSellLimits(
+            this.weiAmount,
+            this.buffer,
+            this.description,
+            this.debatingPeriodInMinutes)
+          break;
+        case 'CANCEL_BUY_ORDER':
+          await this.$eth._daoContract.proposalCancelBuyOrder(
+            this.weiAmount,
+            this.description,
+            this.debatingPeriodInMinutes)
+          break;
+        case 'CANCEL_SELL_ORDER':
+          await this.$eth._daoContract.proposalCancelSellOrder(
+            this.weiAmount,
+            this.description,
+            this.debatingPeriodInMinutes)
+          break;
+        case 'ATTACH_TOKEN':
+          await this.$eth._daoContract.proposalAttachToken(
+            this.beneficiary,
+            this.description,
+            this.debatingPeriodInMinutes)
+          break;
+        case 'SET_BANK_ADDRESS':
+          await this.$eth._daoContract.proposalBankAddress(
+            this.beneficiary,
+            this.description,
+            this.debatingPeriodInMinutes)
+          break;
+        case 'RELEVANCE_PERIOD':
+          await this.$eth._daoContract.proposalRelevancePeriod(
+            this.weiAmount,
+            this.description,
+            this.debatingPeriodInMinutes)
+          break;
+        case 'QUEUE_PERIOD':
+          await this.$eth._daoContract.proposalQueuePeriod(
+            this.weiAmount,
+            this.description,
+            this.debatingPeriodInMinutes)
+          break;
+        case 'SET_FEES':
+          await this.$eth._daoContract.proposalFees(
+            this.weiAmount,
+            this.buffer,
+            this.description,
+            this.debatingPeriodInMinutes)
+          break;
+        case 'ADD_ORACLE':
+          await this.$eth._daoContract.proposalAddOracle(
+            this.beneficiary,
+            this.description,
+            this.debatingPeriodInMinutes)
+          break;
+        case 'DISABLE_ORACLE':
+          await this.$eth._daoContract.proposalDisableOracle(
+            this.beneficiary,
+            this.description,
+            this.debatingPeriodInMinutes)
+          break;
+        case 'ENABLE_ORACLE':
+          await this.$eth._daoContract.proposalEnableOracle(
+            this.beneficiary,
+            this.description,
+            this.debatingPeriodInMinutes)
+          break;
+        case 'DELETE_ORACLE':
+          await this.$eth._daoContract.proposalDeleteOracle(
+            this.beneficiary,
+            this.description,
+            this.debatingPeriodInMinutes)
+          break;
+        case 'SET_SCHEDULER':
+          await this.$eth._daoContract.proposalScheduler(
+            this.beneficiary,
+            this.description,
+            this.debatingPeriodInMinutes)
+          break;
+        case 'WITHDRAW_BALANCE':
+          await this.$eth._daoContract.proposalWithdrawBalance(
+            this.description,
+            this.debatingPeriodInMinutes)
+          break;
+      }
     }
   },
   created () {
@@ -99,7 +206,6 @@ export default {
     }
   },
   components: {
-    
   }
 }
 </script>
