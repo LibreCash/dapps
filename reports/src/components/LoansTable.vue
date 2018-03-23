c8aedd476be15cef367b7ee28e2b99ec5b1f453b
decodemybrain <decodemybrain@protonmail.ch>
2018-03-23 10:06:07 +0300

    Add loans data output

--- reports/src/components/LoansTable.vue
+++ reports/src/components/LoansTable.vue
@@ -16,51 +16,23 @@
       :mobile-cards="hasMobileCards"
       :responsive="isResponsive">
       <template slot-scope="props" v-if="!props.row.tempHide">
-        <b-table-column field="report.type" label='Type' centered>
-          {{ props.row.loading ? 'loading...' : props.row.type }}
+        <b-table-column label='Type' centered>
+          {{ props.row.type }}
         </b-table-column>
-        <b-table-column label='Recipient' centered v-if="props.row.recipient == '-'">
-            not set
+        <b-table-column label='Date' centered>
+          {{ props.row.timestamp }}
         </b-table-column>
-        <b-table-column label='Recipient' centered v-else>
-          <a :href="'https://rinkeby.etherscan.io/address/'+props.row.recipient">address</a>
+        <b-table-column label='Deadline' centered>
+          {{ props.row.deadline }}
         </b-table-column>
-        <b-table-column field="report.amount" label='Amount' centered>
+        <b-table-column label='Amount' centered>
           {{ props.row.amount }}
         </b-table-column>
-        <b-table-column field="report.buffer" label='Buffer' centered>
-          {{ props.row.buffer }}
+        <b-table-column label='Margin' centered>
+          {{ props.row.margin }}
         </b-table-column>
-        <b-table-column label='Description' centered>
-          {{ props.row.description }}
-        </b-table-column>
-        <b-table-column label='Votes' centered>
-          {{ props.row.yea }} / {{ props.row.nay }}
-        </b-table-column>
-         <b-table-column label='Deadline' centered>
-          {{ props.row.deadline }}
-          <span v-if="props.row.deadlineUnix <= curBlockchainTime">
-            outdated
-          </span>
-        </b-table-column>
-        <b-table-column label='Actions' centered>
-          <router-link :to="{name: 'DAO Proposal', params: { id: props.row.id }}" tag="button"><i class="mdi mdi-account-card-details"></i></router-link>
-          <span v-if="!props.row.votingData.voted && (props.row.deadlineUnix > curBlockchainTime) && !props.row.loading">
-            <button v-on:click="vote(props.row, true)"><i class="mdi mdi-check"></i></button>
-            <button v-on:click="vote(props.row, false)"><i class="mdi mdi-close"></i></button>
-          </span>
-          <span v-else-if="props.row.deadlineUnix <= curBlockchainTime">
-            <button v-on:click="execute(props.row)"><i class="mdi mdi-console"></i></button>
-          </span>
-          <span v-else-if="props.row.votingData.voted">
-            voted
-          </span>
-          <span v-else>
-            loading
-          </span>
-          <span v-if="isOwner">
-            <button v-on:click="block(props.row)"><i class="mdi mdi-block-helper"></i></button>
-          </span>
+        <b-table-column label='Status' centered>
+          {{ props.row.status }}
         </b-table-column>
       </template>
       </b-table>
@@ -71,117 +43,9 @@
 export default {
   props: ['tableData'],
   methods: {
-    async vote (row, support) {
-      let 
-        id = row.id,
-        votingData = row.votingData
-
-      row.loading = true
-      try {
-        let 
-          txHash = await this.$eth.daoContract.vote(id, support),
-          message = (await this.$eth.isSuccess(txHash)) ? 'vote tx ok' : 'vote tx failed'
-          alert(message)
-      }catch(e) {
-        alert(this.$eth.getErrorMsg(e)) 
-      }
-      
-      row.loading = false
-
-      try {
-        let voteData =  await this.$eth.getVotingData(row.id);
-        row = {
-          yea: +voteData.yea / 10 ** 18,
-          nay: +voteData.nay / 10 ** 18,
-          votingData: voteData // Check that we needed it
-        }
-      } catch(e) {
-        alert(this.$eth.getErrorMsg(e))
-      }
-      row.loading = false
-    
-    },
-    async block (row) {
-      row.loading = true
-
-      try {
-      let 
-        txHash = await this.$eth.daoContract.blockingProposal(row.id)
-        message = (await this.$eth.isSuccess(txHash)) ? 'block tx ok' : 'block tx failed'
-        alert(message);
-      }catch(e) {
-        alert(this.$eth.getErrorMsg(e))
-      }
-      row.loading = false
-    },
-
-    async execute(row) {
-      row.loading = true
-      
-      let 
-        id = row.id
-      
-      try {
-        let txHash = await this.$eth.daoContract.executeProposal(id)
-        message = (await this.$eth.isSuccess(txHash)) ? 'Execute proposal successful' : 'Execute proposal failed'
-        alert(message)
-      } catch(e) {
-        alert(this.$eth.getErrorMsg(e))
-      }
-      row.loading = false
-      
-    },
     async updateBlockTime() {
       this.curBlockchainTime = +(await this.$eth.getLatestBlockTime())
-    },
-
-    startUpdatingTime() {
-      this.curBlockchainTime = 0
-      this.updatingTicker = setInterval(() => {
-        this.curBlockchainTime++
-      }, 1000)
-      this.updatingBlockData = setInterval(() => {
-        this.updateBlockTime()
-      }, 10 * 60 * 1000 /* 10 minutes */)
-      this.updateBlockTime()
-      this.numProposals = -1
-      this.updateTableData = setInterval(async () => {
-        var numProposals = +(await this.$eth.daoContract.numProposals())
-        if (numProposals !== this.numProposals && this.numProposals !== -1) {
-          this.needUpdate = true
-          console.log('you need update')
-          clearInterval(this.updateTableData)
-        }
-        if (this.numProposals === -1) {
-          this.numProposals = numProposals
-        }
-      }, 60 * 1000)
     }
-  },
-  created () {
-    this.startUpdatingTime()
-    this.$eth.daoContract.owner().then((owner) => {
-      this.contractOwner = owner
-      var loginChecker = setInterval(() => {
-        if (this.$eth.yourAccount != null) {
-          clearInterval(loginChecker)
-          if (owner === this.$eth.yourAccount) {
-            this.isOwner = true
-          } else {
-            this.isOwner = false
-          }
-        }
-      }, 1000)
-    })
-  },
-  destroyed () {
-    let intrevals = [
-      this.updatingTicker,
-      this.updatingBlockData,
-      this.updateTableData
-    ]
-
-    intrevals.forEach((interval) => clearInterval(interval))
   },
   data () {
     return {
