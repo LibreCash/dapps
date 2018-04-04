cae0bc37bc023dd82e351bdc4bf833794fbaeac1
Nikolay Nozdrin-Plotnitsky <nozdrin.plotnitsky@gmail.com>
2018-04-04 11:41:13 +0300

    New page - Deposit

---
+++ reports/src/pages/Deposit.vue
@@ -0,0 +1,33 @@
+<template>
+  <div>
+    <section class="allMain">
+      <div class="h2-contain">
+        <h2 class="subtitle">Deposit</h2>
+      </div>
+      <br>
+      <div class="table-padding">
+        
+      </div>
+      <b-loading :active.sync="isLoading" :canCancel="true"></b-loading>
+    </section>
+  </div>
+</template>
+
+
+
+<script>
+export default {
+  data () {
+    return {
+      isLoading: false
+    }
+  },
+  created () {
+    try {
+
+    } catch (err) {
+      console.log(err)
+    }
+  }
+}
+</script>
