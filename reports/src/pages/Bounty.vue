45c97c08e66c62c4fd9c3d02e43826fa32854d05
evercodex <evercodex@gmail.com>
2018-04-17 21:28:50 +0300

    wip

--- reports/src/pages/Bounty.vue
+++ reports/src/pages/Bounty.vue
@@ -14,12 +14,6 @@
           <div class="column is-narrow">
             <router-link :to="{ path: '/dao/new_offer' }" class="button is-primary">New Targets</router-link>
           </div>
-
-
-        </div>
-        <br>
-        <div v-if="targetCount == 0">
-          No loans for selected filter
         </div>
 
         <b-table
@@ -29,8 +23,6 @@
           :striped="isStriped"
           :narrowed="isNarrowed"
           :loading="tableLoading"
-          :per-page="perPage"
-          :current-page.sync="currentPage"
           :mobile-cards="hasMobileCards"
           :responsive="isResponsive">
           <template slot-scope="props" v-if="!props.row.tempHide">
@@ -45,42 +37,28 @@
               {{ props.row.name }}
             </b-table-column>
             <b-table-column label='ABI' centered>
-              button get abi
+                <b-tooltip label="Get ABI" type="is-dark" position="is-bottom">
+                    <button class="button" v-on:click="showABI(props.row)"><i class="mdi mdi-json"></i></button>
+                </b-tooltip>
             </b-table-column>
             <b-table-column label='Mist import' centered>
-              button get script
+                <b-tooltip label="Get ABI" type="is-dark" position="is-bottom">
+                    <button class="button" v-on:click="showMist(props.row)"><i class="mdi mdi-currency-eth"></i></button>
+                </b-tooltip>
             </b-table-column>
             <b-table-column label='Hacked' centered>
-              No
+                {{ props.row.hacked }}
                 <b-tooltip label="Update info" type="is-dark" position="is-bottom">
-                    <button v-on:click="update(props.row)"><i class="mdi mdi-refresh"></i></button>
+                    <button class="button" v-on:click="update(props.row)"><i class="mdi mdi-refresh"></i></button>
                 </b-tooltip>
             </b-table-column>            
             <b-table-column label='Actions' centered>
                 <b-tooltip label="Claim reward" type="is-dark" position="is-bottom">
-                    <button v-on:click="claim(props.row)"><i class="mdi mdi-target"></i></button>
+                    <button class="button" v-on:click="claim(props.row)" :disabled="!props.row.hacked"><i class="mdi mdi-target"></i></button>
                 </b-tooltip>
             </b-table-column>
           </template>
         </b-table>
-        <div class="columns">
-          <div class="column is-narrow">
-            <b-tooltip label="Items on page">
-              <b-field>
-                <b-select v-model="perPage" @input="loadTargets()">
-                  <option value="3">3</option>
-                  <option value="5">5</option>
-                  <option value="10">10</option>
-                  <option value="15">15</option>
-                  <option value="25">25</option>
-                  <option value="50">50</option>
-                  <option value="100">100</option>
-                  <option value="300">300</option>
-                </b-select>
-              </b-field>
-            </b-tooltip>
-          </div>
-        </div>
       </div>
     </section>
   </div>
@@ -89,6 +67,28 @@
 <script>
 import AddressBlock from '@/components/AddressBlock'
 import Config from '@/config'
+
+const modalABI = {
+    props: ['title', 'data'],
+    template: `
+        <form action=''>
+            <div class="modal-card" style="width: auto">
+                <header class="modal-card-head">
+                    <p class="modal-card-title">{{ title }}</p>
+                </header>
+                <section class="modal-card-body">
+                    <b-field label="Data">
+                        <b-input type="textarea" :value="data"></b-input>
+                    </b-field>
+                </section>
+                <footer class="modal-card-foot">
+                    <button class="button" type="button" @click="$parent.close()">Close</button>
+                </footer>
+            </div>
+        </form>
+    `
+}
+
 export default {
   data () {
     return {
@@ -99,29 +99,80 @@ export default {
       tableLoading: false,
       hasMobileCards: true,
       isResponsive: true,
-      currentPage: 1,
-      perPage: 5,
       curBlockchainTime: 0,
       bountyBankAddress: '',
       bountyExchangerAddress: '',
       searchData: [],
-      isSimple: false,
-      isRounded: true,
-      paginationOrder: 'is-centered',
       defaultAddress: '',
-      pages: [1],
-      vpage: 1,
-      ethType: 'ETH',
       isActive: true,
       isUsed: false,
       isCompleted: false,
       isMine: false,
       targetCount: 0,
-      perPage: 10,
       curBlockchainTime: 0
+
     }
   },
   methods: {
+    getABI (row) {
+        let ABI;
+        if (row.name == "LibreCash") ABI = Config.bounty.bank.targets.token.abi;
+        else if (row.name == "ComplexBank") ABI = Config.bounty.bank.targets.bank.abi;
+        else if (row.name == "ComplexExchanger") ABI = Config.bounty.exchanger.targets.exchanger.abi;
+        else ABI = "ABI not fount for this type of target";
+        return ABI;
+    },
+    showABI (row) {
+        this.$modal.open({
+            parent: this,
+            component: modalABI,
+            hasModalCard: true,
+            props: {
+                title: 'Contract ABI',
+                data: JSON.stringify(this.getABI(row))
+            }
+        })
+    },
+    showMist (row) {
+        let ABI = JSON.stringify(this.getABI(row));
+        let address = row.address;
+        let name = row.name;
+        let mistData = 
+`CustomContracts.insert({
+    address: "${address}",
+    name: "_${name}",
+    jsonInterface: ${ABI}
+});`;
+        this.$modal.open({
+            parent: this,
+            component: modalABI,
+            hasModalCard: true,
+            props: {
+                title: 'Mist import script',
+                data: mistData
+            }
+        })
+    },
+    async claim (row) {
+      try {
+        this.tableLoading = true;
+        let 
+          txHash = await row.bountyContract.claim(row.address);
+        let
+          result = await this.$eth.isSuccess(txHash) ? 'Success claim transaction' : 'Failed claim transaction';
+
+        alert(result); // Replace it to notify
+        this.tableLoading = false;
+      } catch(err) {
+        alert(this.$eth.getErrorMsg(err));
+        this.tableLoading = false;
+      }
+    },
+    update (row) {
+        row.contract.checkInvariant(this.defaultAddress).then((notHacked) => {
+            row.hacked = !notHacked;
+        });
+    },
     async loadTargets (e) {
       this.searchData = [];
 
@@ -141,26 +192,22 @@ export default {
               id: i,
               address: bankTarget,
               name: '...',
-              contract: this.$libre.getContract(Config.bounty.bank.targets.bank.abi, bankTarget)
-              //recipient: this.$eth.isZeroAddress(loan.recipient) ? '-' : loan.recipient,
-              //timestampUnix: loan.timestamp,
-              //timestamp: new Date(loan.timestamp * 1000).toLocaleString(),
-              //period: new Date((loan.timestamp + loan.period) * 1000).toLocaleString(),
-              //amount: this.$eth.fromWei(loan.amount),
-              //margin: this.$eth.fromWei(loan.margin),
-              //refund: this.$eth.fromWei(loan.refund),
-              //status: loan.status
+              contract: this.$libre.getContract(Config.bounty.bank.targets.bank.abi, bankTarget),
+              hacked: false,
+              bountyContract: this.$libre.bounty.bank
           });
           let searchDataLength = this.searchData.length;
-          console.log(this.searchData[searchDataLength - 1].contract);
           
           this.searchData[searchDataLength - 1].contract.targetName().then((_name) => {
               this.searchData[searchDataLength - 1].name = _name;
               if (_name == "LibreCash") {
                 this.searchData[searchDataLength - 1].contract = this.$libre.getContract(Config.bounty.bank.targets.token.abi, bankTarget);
               }
-              
-          })
+            
+          });
+          this.searchData[searchDataLength - 1].contract.checkInvariant(this.defaultAddress).then((notHacked) => {
+              this.hacked = !notHacked;
+          });
         }
       } catch (err) {
         console.log(err)
