4b94e0f80e0932248a82c1100272d3ce1d90bd66
evercodex <evercodex@gmail.com>
2018-04-27 17:11:47 +0300

    i18n WIP on integration

--- src/main.js
+++ src/main.js
@@ -6,20 +6,34 @@ import router from './router'
 import './plugins/buefy'
 import ETH from './plugins/eth'
 import Libre from './plugins/libre'
+import VueI18n from 'vue-i18n'
 
 Vue.config.productionTip = false
 
 Vue.use(ETH, {})
 Vue.use(Libre, {})
+Vue.use(VueI18n)
 
 router.beforeEach((to, from, next) => {
   document.title = to.meta.title
   next()
 })
 
+const i18n = new VueI18n({
+  locale: 'en',
+  messages: {
+    en: {
+      message: {
+        hello: "yo"
+      }
+    }
+  },
+})
+
 new Vue({
   el: '#app',
   router,
+  i18n,
   template: '<App/>',
   components: { App }
 })
